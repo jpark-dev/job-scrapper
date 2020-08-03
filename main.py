3008c35019fb7c9357d78545c964f2950664b955
Jasom Park <zeipar@gmail.com>
2020-08-03 16:12:47 -0700

    Extract last page number of the search result.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 target_url = "https://ca.indeed.com/jobs?q=Junior+Developer+-senior%2C+-sr&l=Vancouver%2C+BC&limit=50&radius=25"
 
 r = requests.get(target_url)
 
-print(r)
-print(r.text)
+soup = BeautifulSoup(r.text, "html.parser")
+
+pagination = soup.find("div", {"class": "pagination"})
+
+pages = pagination.find_all("a")
+
+spans = []
+
+for page in pages:
+    spans.append(page.find("span"))
+
+print(spans[:-1])  
