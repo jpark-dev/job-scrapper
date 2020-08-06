0e2997fee736194554f9465db06d7d1a8da3d44a
Jasom Park <zeipar@gmail.com>
2020-08-05 23:46:41 -0700

    Began creating scraper for job listings in stackoverflow.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from indeed import get_jobs as get_indeed_jobs
+from sof import get_jobs as get_sof_jobs
 
-indeed_jobs = get_indeed_jobs()
+# indeed_jobs = get_indeed_jobs()
+sof_jobs = get_sof_jobs()
 
-print(indeed_jobs)
+tot_jobs = len(indeed_jobs) + len(sof_jobs)
 
-print(f"Scrapper Done! Scraped {len(indeed_jobs)} jobs. Great job, Jason!")
+print(f"Scrapper Done! Scraped {tot_jobs} jobs. Great job, Jason!")
