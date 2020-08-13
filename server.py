51d98ae6f4710bfc0a96465f4544eb91231947f4
Jasom Park <zeipar@gmail.com>
2020-08-12 23:38:19 -0700

    Added route for report page, accepts user input, lowercases all user input, and redirects to root if no word is passed.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask("scrapper!!!")
 
@@ -6,4 +6,11 @@ app = Flask("scrapper!!!")
 def home():
     return render_template("home.html")
 
-    
+@app.route("/report")
+def report():
+    word = request.args.get("word")
+    if word:
+        word = word.lower()
+    else:
+        return redirect("/")
+    return render_template("report.html", searchWord=word)
